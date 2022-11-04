990c138b9bd9963e5656596bb15ed9c76a54315f
developerlenin <justlenin9095@gmail.com>
2022-11-04 19:10:14 +0530

    new

---
+++ index.js
@@ -0,0 +1,7 @@
+function ans() {
+    const param = document.createElement("p");
+    const br =document.createElement("br");
+    param.append(inputt)
+    var inputt= document.getElementById("input").value;
+    document.getElementById("para").append(inputt,br,br);
+  }
